8913d11977770cc0c08393cdfa8b0c667fdc8d2b
Rickasikhan <kasrakhalaj44@gmail.com>
2025-12-07 22:17:51 +0330

    chore: Upgrade dependencies and fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,6 @@
 const nextConfig = {
     /* config options here */
     reactStrictMode: true,
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
